044c42f0ad58ceb90ed1fbb15cf9859e11023af3
salman-23 <66154732+salman-23@users.noreply.github.com>
2021-03-25 11:27:00 +0300

    user-profile

--- app.js
+++ app.js
@@ -1,26 +1,23 @@
-const express = require("express");
 const db = require("./db/models");
+const express = require("express");
 const cors = require("cors");
+const path = require("path");
+const userRoutes = require("./routes/users");
 const passport = require("passport");
 const { localStrategy, jwtStrategy } = require("./middleware/passport");
 
-const userRoutes = require("./routes/users");
-
-const path = require("path");
-
 const app = express();
-app.use(express.json());
 app.use(cors());
-
-// Passport Setup
+app.use(express.json());
 app.use(passport.initialize());
 passport.use(localStrategy);
 passport.use(jwtStrategy);
 
 //Routers
-app.use(userRoutes);
+app.use("/user", userRoutes);
 
 app.use("/media", express.static(path.join(__dirname, "media")));
+
 app.use((req, res, next) => {
   const error = new Error("Path Not Found");
   error.status = 404;
@@ -32,7 +29,10 @@ app.use((err, req, res, next) => {
   res.json({ message: err.message || "Internal Server Error" });
 });
 
+//db.sequelize.sync();
 db.sequelize.sync({ alter: true });
+// db.sequelize.sync({ force: true });
+
 app.listen(8000, () => {
   console.log("The application is running on localhost:8000");
 });
